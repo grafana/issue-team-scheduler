0874cb68e75bf12c1b4e677e15a369945985e920
Mauro Stettler <mauro.stettler@gmail.com>
2024-05-31 15:14:57 +0200

    spelling
    Co-authored-by: Dimitar Dimitrov <dimitar.dimitrov@grafana.com>

--- pkg/icassigner/busyness/busyness.go
+++ pkg/icassigner/busyness/busyness.go
@@ -171,7 +171,7 @@ func (b *githubBusynessClient) getBusyness(ctx context.Context, since time.Time,
 				log.Printf("%s: Issue increases busyness because it has been closed at %s which is after %s: %s\n", member, i.GetClosedAt().String(), since.String(), i.GetTitle())
 				busyness++
 			} else {
-				log.Printf("%s: Issue doesn't increase busyiness because it has been closed at %s which is before %s: %s\n", member, i.GetClosedAt().String(), since.String(), i.GetTitle())
+				log.Printf("%s: Issue doesn't increase busyness because it has been closed at %s which is before %s: %s\n", member, i.GetClosedAt().String(), since.String(), i.GetTitle())
 			}
 		default:
 			log.Printf("%s: Issue doesn't increase busyness because it has an unknown state (%s): %s\n", member, i.GetState(), i.GetTitle())
